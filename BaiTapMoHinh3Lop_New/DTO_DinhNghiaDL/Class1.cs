3d236eb89e1ba2fd0fbea3d43b16fa5b145c003e
THÀNH LONG VÕ <longvophuongnguyen@gmail.com>
2018-11-18 21:27:09 +0700

    Xay dung xong lop DTO

--- BaiTapMoHinh3Lop_New/DTO_DinhNghiaDL/Class1.cs
+++ BaiTapMoHinh3Lop_New/DTO_DinhNghiaDL/Class1.cs
@@ -8,5 +8,20 @@ namespace DTO_DinhNghiaDL
 {
     public class Class1
     {
+        private string _maNhom;
+        private string _tenNhomTruong;
+        private string _chuNangNhom;
+
+        public string MaNhom { get => _maNhom; set => _maNhom = value; }
+        public string TenNhomTruong { get => _tenNhomTruong; set => _tenNhomTruong = value; }
+        public string ChuNangNhom { get => _chuNangNhom; set => _chuNangNhom = value; }
+
+        public Class1() { }
+        public Class1(string Id,string LeaderName,string Function)
+        {
+            this._maNhom = Id;
+            this._tenNhomTruong = LeaderName;
+            this._chuNangNhom = Function;
+        }
     }
 }
