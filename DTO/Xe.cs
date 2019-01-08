33ce731b4dafda8c434ac462b24e9b5cc13e1677
nam <tphoainam@gmail.com>
2019-01-08 15:11:02 +0700

    Update Xe.cs

--- DTO/Xe.cs
+++ DTO/Xe.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace DTO
 {
-    class Xe
+    public class Xe
     {
+        private string _maXe;
+        private string _tenXe;
+        private string _loaiXe;
+        private string _soDangKy;
+
+        public string MaXe { get => _maXe; set => _maXe = value; }
+        public string TenXe { get => _tenXe; set => _tenXe = value; }
+        public string LoaiXe { get => _loaiXe; set => _loaiXe = value; }
+        public string SoDangKy { get => _soDangKy; set => _soDangKy = value; }
+        public Xe() { }
+        public Xe(string maXe, string tenXe, string loaiXe, string soDangKy)
+        {
+            this.MaXe = maXe;
+            this.TenXe = tenXe;
+            this.LoaiXe = loaiXe;
+            this.SoDangKy = soDangKy;
+        }
     }
 }
