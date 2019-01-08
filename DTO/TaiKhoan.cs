0f90452e9f835f31f6c937bb564f6f0b4773afac
nam <tphoainam@gmail.com>
2019-01-08 14:47:42 +0700

    Tạo lớp định nghĩa taikhoan

---
+++ DTO/TaiKhoan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class TaiKhoan
+    {
+        private string _tenDangNhap;
+        private string _matKhau;
+        private string _phanQuyen;
+
+        public string TenDangNhap { get => _tenDangNhap; set => _tenDangNhap = value; }
+        public string MatKhau { get => _matKhau; set => _matKhau = value; }
+        public string PhanQuyen { get => _phanQuyen; set => _phanQuyen = value; }
+        public TaiKhoan() { }
+        public TaiKhoan(string tenDangNhap,string matKhau,string phanQuuen)
+        {
+            this.TenDangNhap = tenDangNhap;
+            this.PhanQuyen = phanQuuen;
+            this.MatKhau = matKhau;
+        }
+    }
+}
