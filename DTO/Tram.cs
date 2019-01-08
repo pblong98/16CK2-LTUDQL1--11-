2f3668ad6167d29a640618933749e3625c2935c4
nam <tphoainam@gmail.com>
2019-01-08 15:05:54 +0700

    Update Tram.cs

--- DTO/Tram.cs
+++ DTO/Tram.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace DTO
 {
-    class Tram
+    public class Tram
     {
+        private string _maTram;
+        private string _tenTram;
+        private string _diaDiem;
+        private string _thuTu;
+
+        public string MaTram { get => _maTram; set => _maTram = value; }
+        public string TenTram { get => _tenTram; set => _tenTram = value; }
+        public string DiaDiem { get => _diaDiem; set => _diaDiem = value; }
+        public string ThuTu { get => _thuTu; set => _thuTu = value; }
+
+        public Tram() { }
+
+        public Tram(string maTram,string tenTram,string diaDiem,string thuTu)
+        {
+            this.MaTram = maTram;
+            this.TenTram = tenTram;
+            this.ThuTu = thuTu;
+            this.DiaDiem = diaDiem;
+        }
     }
 }
