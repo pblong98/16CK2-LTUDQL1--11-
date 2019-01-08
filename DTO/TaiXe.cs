ab4b31422a7f679e27057dc78488c4129a0f71f4
nam <tphoainam@gmail.com>
2019-01-08 14:53:06 +0700

    tạo lớp định nghĩa cho tài xế

--- DTO/TaiXe.cs
+++ DTO/TaiXe.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace DTO
 {
-    class TaiXe
+    public class TaiXe
     {
+        private string _ma;
+        private string _tenTaiXe;
+        private string _bangLai;
+
+        public string Ma { get => _ma; set => _ma = value; }
+        public string TenTaiXe { get => _tenTaiXe; set => _tenTaiXe = value; }
+        public string BangLai { get => _bangLai; set => _bangLai = value; }
+        public TaiXe() { }
+        public TaiXe( string ma,string tenTaiXe,string bangLai)
+        {
+            this.BangLai = bangLai;
+            this.Ma = ma;
+            this.TenTaiXe = tenTaiXe;
+        }
     }
 }
